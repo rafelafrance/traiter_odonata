c2d4531d8df6bec344137367e3a814ab2f8e5f31
rafe <raphael.lafrance@gmail.com>
2020-11-30 12:58:04 -0500

    Return multiple traits from color phrase matchers

--- src/matchers/color.py
+++ src/matchers/color.py
@@ -2,6 +2,9 @@
 
 from ..pylib.util import DASH, MISSING, REPLACE, TRAIT_STEP
 
+COLORS = ['color', 'color_modifier']
+PARTS = ['part', 'part_location', 'fly']
+
 
 def color(span):
     """Enrich a color match."""
@@ -13,25 +16,57 @@ def color(span):
 
 def color_phrase(span):
     """Enrich a phrase match."""
-    data = {}
-    colors, parts, reverse = [], [], False
+    data = []
+    color_start, color_end, colors = -1, -1, []
+    part_start, part_end, parts, locations = -1, -1, [], []
+    missing, reverse = False, False
 
-    for token in span:
+    for i, token in enumerate(span):
         label = token.ent_type_
-        if label in ('color', 'color_modifier'):
-            colors.append(REPLACE.get(token.lower_, token.lower_))
-        elif label in ['part', 'part_location']:
-            parts.append(REPLACE.get(token.lower_, token.lower_))
+        value = REPLACE.get(token.lower_, token.lower_)
+
+        if label in COLORS:
+            color_start = i if color_start < 0 else color_start
+            color_end = i + 1
+            colors.append(value)
+        elif label in PARTS:
+            part_start = i if part_start < 0 else part_start
+            part_end = i + 1
+            if label == 'part_location':
+                locations.append(value)
+            else:
+                parts.append(value)
         elif token.lower_ in MISSING:
-            data['missing'] = True
+            missing = True
         elif token.pos_ in ('ADP',):
             reverse = True
 
-    colors = reversed(colors) if reverse else colors
-    data['color'] = '-'.join(colors)
+    part = ' '.join(parts)
+    location = ' '.join(locations)
 
-    if parts:
-        data['part'] = ' '.join(parts)
+    colors = reversed(colors) if reverse else colors
+    color_data = {
+        '_span': span[color_start:color_end],
+        '_label': 'color',
+        'color': '-'.join(colors),
+    }
+    if part:
+        color_data['body_part'] = part
+    if location:
+        color_data['location'] = location
+    if missing:
+        color_data['missing'] = True
+    data.append(color_data)
+
+    if part:
+        part_data = {
+            '_span': span[part_start:part_end],
+            '_label': 'body_part',
+            'body_part': part,
+        }
+        if location:
+            part_data['location'] = location
+        data.append(part_data)
 
     return data
 
@@ -43,11 +78,11 @@ COLOR = {
             'on_match': color,
             'patterns': [
                 [
-                    {'ENT_TYPE': {'IN': ['color', 'color_modifier']}, 'OP': '*'},
+                    {'ENT_TYPE': {'IN': COLORS}, 'OP': '*'},
                     {'TEXT': {'IN': DASH}, 'OP': '?'},
-                    {'ENT_TYPE': {'IN': ['color', 'color_modifier']}, 'OP': '+'},
+                    {'ENT_TYPE': {'IN': COLORS}, 'OP': '+'},
                     {'TEXT': {'IN': DASH}, 'OP': '?'},
-                    {'ENT_TYPE': {'IN': ['color', 'color_modifier']}, 'OP': '*'},
+                    {'ENT_TYPE': {'IN': COLORS}, 'OP': '*'},
                 ],
             ],
         },
@@ -56,19 +91,21 @@ COLOR = {
             'on_match': color_phrase,
             'patterns': [
                 [
-                    {'ENT_TYPE': {'IN': ['color', 'color_modifier']}, 'OP': '+'},
+                    {'ENT_TYPE': {'IN': COLORS}, 'OP': '+'},
                     {'POS': {'IN': ['ADP']}, 'OP': '?'},
                     {'ENT_TYPE': 'color', 'OP': '+'},
                 ],
                 [
                     {'LOWER': {'IN': MISSING}, 'OP': '?'},
-                    {'ENT_TYPE': {'IN': ['part', 'part_location']}, 'OP': '+'},
-                    {'ENT_TYPE': {'IN': ['color', 'color_modifier']}, 'OP': '+'},
+                    {'POS': {'IN': ['ADJ']}, 'OP': '?'},
+                    {'ENT_TYPE': {'IN': PARTS}, 'OP': '+'},
+                    {'ENT_TYPE': {'IN': COLORS}, 'OP': '+'},
                 ],
                 [
                     {'LOWER': {'IN': MISSING}, 'OP': '?'},
-                    {'ENT_TYPE': {'IN': ['color', 'color_modifier']}, 'OP': '+'},
-                    {'ENT_TYPE': {'IN': ['part', 'part_location']}, 'OP': '+'},
+                    {'POS': {'IN': ['ADJ']}, 'OP': '?'},
+                    {'ENT_TYPE': {'IN': COLORS}, 'OP': '+'},
+                    {'ENT_TYPE': {'IN': PARTS}, 'OP': '+'},
                 ],
             ],
         },
