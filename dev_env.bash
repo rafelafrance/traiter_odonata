ac69e95698349a4a60e7b84ab3ca76e2493bbd4a
rafe <raphael.lafrance@gmail.com>
2022-03-17 11:34:27 -0400

    More git weirdness

--- dev_env.bash
+++ dev_env.bash
@@ -12,7 +12,6 @@ source ./.venv/bin/activate
 python -m pip install --upgrade pip setuptools wheel
 if [ -f requirements.txt ]; then pip install -r requirements.txt; fi
 
-pip install -U spacy[cuda111,transformers,lookups]
 python -m spacy download en_core_web_sm
 
 pip install -e ../traiter
